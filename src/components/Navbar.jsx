98811b877bebb8b353dea9aff007d788ab6aeeb4
“Wai <waye14671@gmail.com>
2026-05-07 18:21:51 +0630

    nav responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,24 @@
-  
+import { HiMenu , HiX} from "react-icons/hi";
+import { useState } from "react";
 
 function Navbar (){
+
+    const [isOpen , setIsOpen] = useState(false);
+
+    const btnClick = () =>{
+            setIsOpen(!isOpen);
+            
+        }
+
     return (
-        <nav className="w-full border-b-2 border-be-violet-100 bg-background">
+
+        <nav className="w-full border-b-1 border-be-violet-100 bg-background">
             <div className="container-custom flex items-center justify-between">
                 <div className="text-4xl tracking-tight font-bold">
                     LOOO
                 </div>
 
-                <div className="flex gap-6">
+                <div className="hidden md:flex gap-6">
                     <a href="#" className="nav-link border-b-2 border-primary">Home</a>
                     <a href="#" className="nav-link ">Teachers</a>
                     <a href="#" className="nav-link ">Offers</a>
@@ -19,6 +29,37 @@ function Navbar (){
                     <button className="secondary-btn">Sign in</button>
                     <button className="primary-btn">Free Trial</button>
                 </div>
+
+                <div className="text-primary text-2xl md:hidden" onClick={btnClick}>
+                    <HiMenu/>
+                </div>
+
+                <div className={`fixed
+                                flex 
+                                flex-col  
+                                absolute 
+                                top-0 
+                                right-0 
+                                text-center
+                                h-[100%] 
+                                w-8/12
+                                bg-background
+                                  text-black 
+                                  transition-all 
+                                  duration-300
+                                  ${isOpen ? "translate-x-0" : "translate-x-full"}`}>
+
+                    <button className="absolute top-0 right-0 bg-primary px-4 py-4 text-white text-lg rounded-bl-4xl rounded-tl-4xl" 
+                            onClick={()=>setIsOpen(false)}>
+                                <HiX/>
+                    </button>
+
+                    <a href="#" className=" bg-amber-200 py-5 text-primary text-base">Home</a>
+                    <a href="#" className="  py-5 text-primary text-base">Teachers</a>
+                    <a href="#" className="  py-5 text-primary text-base">Offers</a>
+                    <a href="#" className="  py-5 text-primary text-base">Contact</a>
+                </div>
+                
             </div>
         </nav>
     )
